fbb773eb1392aa333437a20647b6222e0caa4d55
ChungKin44 <44889162+ChungKin44@users.noreply.github.com>
2023-08-28 11:57:28 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,194 @@
+import os
+import cv2
+import xml.etree.ElementTree as ET
+from pathlib import Path
+import glob
+import csv
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
+from keras.preprocessing.image import img_to_array, array_to_img
+
+root_dir = "C:/Users/willi/OneDrive/桌面/Dataset/train"
+
+# Machine Learning
+import torch
+from torch import nn
+from torch.utils.data import DataLoader, Dataset
+from torchvision import datasets, transforms
+
+
+def convert_label(lab):
+    re = []
+    for string in lab:
+        result = []
+        length = len(string)
+        for str in string:
+            if str == 'leaf':
+                result.append(0)
+            elif str == 'stem':
+                result.append(1)
+            elif str == 'soil':
+                result.append(2)
+        if len(result) == length:
+            re.append(result)
+    return re
+
+
+def normalize(img_numpy_array):
+    return img_numpy_array / 255.0
+
+
+def standardization(imagelist_np, BBoxes_np):
+    X_train_mean = np.mean(imagelist_np, axis=0)
+    X_train = imagelist_np - X_train_mean
+    y_train = np.array(BBoxes_np)
+    return X_train, y_train
+
+
+def parse_xml(xml_path):
+    tree = ET.parse(xml_path)
+    root = tree.getroot()
+
+    bounding_boxes = []
+    labels = []
+    for obj in root.findall('object'):
+        name = obj.find('name').text
+        bndbox = obj.find('bndbox')
+        xmin = int(bndbox.find('xmin').text)
+        ymin = int(bndbox.find('ymin').text)
+        xmax = int(bndbox.find('xmax').text)
+        ymax = int(bndbox.find('ymax').text)
+        bounding_boxes.append([xmin, ymin, xmax, ymax])
+        labels.append(name)
+    size = root.find('size')
+    width = int(size.find('width').text)
+    height = int(size.find('height').text)
+
+    return bounding_boxes, labels, width, height
+
+
+import os
+import glob
+import csv
+import xml.etree.ElementTree as ET
+
+def xml_to_csv(root_dir):
+    bbox = []
+    labels = []
+    for xml_file in glob.glob(root_dir + '/*.xml'):
+        if os.path.exists(xml_file):
+            bboxes, lbls, width, height = parse_xml(xml_file)
+            bbox.append(bboxes)
+            labels.append(lbls)
+            label_in_num = convert_label(lbls)
+
+    print(len(bbox))
+    print(len(labels))
+    csv_file = os.path.join(root_dir, 'annotations.csv')
+    with open(csv_file, 'w', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow(['filename', 'width', 'height', 'label', 'xmin', 'ymin', 'xmax', 'ymax'])
+
+        for i in range(len(bbox)):
+           for j in range(len(bbox[i])):
+                writer.writerow([os.path.basename(xml_file), width, height, labels[i][j],bbox[i][j][0],bbox[i][j][1],bbox[i][j][2],bbox[i][j][3]])
+
+    print(f"CSV file saved at: {csv_file}")
+
+
+
+xml_to_csv(root_dir)
+
+
+
+
+
+
+
+"""
+# check quantity of image and labels
+print("Total Read Image and label = ", image_count, label_count)
+# Print image shapes and bounding boxes
+img_shape = num_imagelist[0].shape
+print(f'Image {0 + 1} shape: {img_shape}')
+print(f'Bounding boxes for image {0 + 1} shape: {bboxes.shape}')
+print(len(Label))
+print(len(BBoxes))
+print(len(num_imagelist))
+
+
+# Labeled visualize
+def make_mask(image_dir, save_dir):
+    data = os.listdir(image_dir)
+    temp_data = []
+    for i in data:
+        if i.split('.')[1] == 'json':
+            temp_data.append(i)
+        else:
+            continue
+    for js in temp_data:
+        json_data = json.load(open(os.path.join(image_dir, js), 'r'))
+        shapes_ = json_data['shapes']
+        mask = Image.new('P', Image.open(os.path.join(image_dir, js.replace('json', 'png'))).size)
+        for shape_ in shapes_:
+            label = shape_['label']
+            points = shape_['points']
+            points = tuple(tuple(i) for i in points)
+            mask_draw = ImageDraw.Draw(mask)
+            mask_draw.polygon(points, fill=CLASS_NAMES.index(label) + 1)
+        mask.save(os.path.join(save_dir, js.replace('json', 'png')))
+
+
+def vis_label(img):
+    img = Image.open(img)
+    img = np.array(img)
+    print(set(img.reshape(-1).tolist()))
+
+
+
+# Network of CNN
+
+class Network(nn.Module):
+
+    def __init__(self, num_classes=21, groups=2):
+        super(Network, self).__init__()
+
+        self.conv = nn.Sequential()
+        self.conv.add_module('conv1_s1', nn.Conv2d(3, 96, kernel_size=11, stride=4, padding=0))
+        self.conv.add_module('relu1_s1', nn.ReLU(inplace=True))
+        # self.conv.add_module('bn1_s1',nn.BatchNorm2d(96))
+        self.conv.add_module('pool1_s1', nn.MaxPool2d(kernel_size=3, stride=2))
+        self.conv.add_module('lrn1_s1', LRN(local_size=5, alpha=0.0001, beta=0.75))
+
+        self.conv.add_module('conv2_s1', nn.Conv2d(96, 256, kernel_size=5, padding=2, groups=groups))
+        self.conv.add_module('relu2_s1', nn.ReLU(inplace=True))
+        # self.conv.add_module('bn2_s1',nn.BatchNorm2d(256))
+        self.conv.add_module('pool2_s1', nn.MaxPool2d(kernel_size=3, stride=2))
+        self.conv.add_module('lrn2_s1', LRN(local_size=5, alpha=0.0001, beta=0.75))
+
+        self.conv.add_module('conv3_s1', nn.Conv2d(256, 384, kernel_size=3, padding=1))
+        self.conv.add_module('relu3_s1', nn.ReLU(inplace=True))
+        # self.conv.add_module('bn3_s1',nn.BatchNorm2d(384))
+
+        self.conv.add_module('conv4_s1', nn.Conv2d(384, 384, kernel_size=3, padding=1, groups=groups))
+        # self.conv.add_module('bn4_s1',nn.BatchNorm2d(384))
+        self.conv.add_module('relu4_s1', nn.ReLU(inplace=True))
+
+        self.conv.add_module('conv5_s1', nn.Conv2d(384, 256, kernel_size=3, padding=1, groups=groups))
+        # self.conv.add_module('bn5_s1',nn.BatchNorm2d(256))
+        self.conv.add_module('relu5_s1', nn.ReLU(inplace=True))
+        self.conv.add_module('pool5_s1', nn.MaxPool2d(kernel_size=3, stride=2))
+
+        self.fc6 = nn.Sequential()
+        self.fc6.add_module('fc6_s1', nn.Linear(256 * 6 * 6, 4096))
+        self.fc6.add_module('relu6_s1', nn.ReLU(inplace=True))
+        self.fc6.add_module('drop6_s1', nn.Dropout(p=0.5))
+
+        self.fc7 = nn.Sequential()
+        self.fc7.add_module('fc7', nn.Linear(4096, 4096))
+        self.fc7.add_module('relu7', nn.ReLU(inplace=True))
+        self.fc7.add_module('drop7', nn.Dropout(p=0.5))
+
+        self.classifier = nn.Sequential()
+        self.classifier.add_module('fc8', nn.Linear(4096, num_classes))   """
