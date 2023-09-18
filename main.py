36001766fa0b1026d3a3ddb3d542ef35d341a05d
ChungKin44 <44889162+ChungKin44@users.noreply.github.com>
2023-09-18 17:15:30 +0800

    Update main.py

--- main.py
+++ main.py
@@ -15,8 +15,10 @@ import torch
 from PIL import Image
 from torch.utils.data import DataLoader, Dataset, random_split
 from torchvision import transforms
+from matplotlib.patches import Polygon
 
 # 2. training and model
+import time
 import unet  # from unet.py
 import torch.nn as nn
 from torch import optim
@@ -40,6 +42,26 @@ def check_cpu():
     return device
 
 
+def show_Unet_result(predict):
+    predict = predict.cpu()
+    output_image = predict[0].detach()
+    output_image = np.array(output_image)
+    output_image = denormalize(output_image)
+    output_image = [[[output_image[i][j][k] for i in range(len(output_image))]
+                     for j in range(len(output_image[0]))]
+                    for k in range(len(output_image[0][0]))]
+
+    output_image = np.clip(output_image, 0, 1)
+
+    # Display the image
+    plt.imshow(output_image)
+    plt.title('After Unet')
+    plt.axis('off')
+    plt.show()
+
+    return 0
+
+
 def convert_label(lab):
     result = []
     for str in lab:
@@ -56,11 +78,28 @@ def normalize(img_numpy_array):  # gray scale
     return img_numpy_array / 255.0
 
 
+def denormalize(img_3d_list):
+    modified_image = []
+
+    for row in img_3d_list:
+        modified_row = []
+        for col in row:
+            modified_col = []
+            for pixel in col:
+                modified_pixel = pixel * -1 if pixel < 0 else pixel * 1
+                modified_col.append(modified_pixel)
+            modified_row.append(modified_col)
+        modified_image.append(modified_row)
+
+    return modified_image
+
+
 def parse_xml(xml_path):
     tree = ET.parse(xml_path)
     root = tree.getroot()
     bounding_boxes = []
     labels = []
+    polygons = []
 
     for obj in root.findall('object'):
         name = obj.find('name').text
@@ -71,41 +110,31 @@ def parse_xml(xml_path):
         ymax = int(bndbox.find('ymax').text)
         bounding_boxes.append([xmin, ymin, xmax, ymax])
         labels.append(name)
+        i = 0
+        polygon = obj.find('polygon')
+        po = []
+        for point in polygon:
+            i += 1
+            if point.tag.startswith('x'):
+                x = float(point.text)
+            elif point.tag.startswith('y'):
+                y = float(point.text)
+                po.append([x, y])
+
+        polygons.append(po)
+
     size = root.find('size')
     width = int(size.find('width').text)
     height = int(size.find('height').text)
-    return bounding_boxes, labels, width, height
+
+    return bounding_boxes, labels, width, height, polygons
 
 
-""" def xml_to_csv(root_dir):                      #XML to CSV format 
-    bbox = []
-    labels = []
-    for xml_file in glob.glob(root_dir + '/*.xml'):
-        if os.path.exists(xml_file):
-            bboxes, lbls, width, height = parse_xml(xml_file)
-            bbox.append(bboxes)
-            labels.append(lbls)
-            label_in_num = convert_label(lbls)
-
-    print(len(bbox))
-    print(len(labels))
-    csv_file = os.path.join(root_dir, 'annotations.csv')
-    with open(csv_file, 'w', newline='') as f:
-        writer = csv.writer(f)
-        writer.writerow(['filename', 'width', 'height', 'label', 'xmin', 'ymin', 'xmax', 'ymax'])
-
-        for i in range(len(bbox)):
-            for j in range(len(bbox[i])):
-                writer.writerow([os.path.basename(xml_file), width, height, labels[i][j], bbox[i][j][0], bbox[i][j][1],
-                                 bbox[i][j][2], bbox[i][j][3]])
-
-    print(f"CSV file saved at: {csv_file}")  """
-
-
-def display_image_with_boxes(image, boxes, lbls):
-    img_with_boxes = image.clone().permute(1, 2, 0).numpy()
-    plt.imshow(img_with_boxes)
-    ax = plt.gca()
+def display_image_with_polygon(data):
+    image = Image.open("C:/Users/willi/PycharmProjects/modelling/Dataset/test/0629_png.rf"
+                       ".6b99a0487d60046d94ad65c6e4cc131b.jpg")
+    image.show()
+    img, label = data[1]
     for bbox in boxes:
         xmin, ymin, xmax, ymax = bbox
         color = 'blue' if lbls == 0 else 'red'  # blus as leaf , red as stem
@@ -114,15 +143,53 @@ def display_image_with_boxes(image, boxes, lbls):
     plt.show()
 
 
-voc_label = {'leaf,stem,soil'}
-dict_labels = dict(zip(voc_label, range(len(voc_label))))
+def polygon_to_mask(labels):
+    labels = labels.tolist()
+    current_label = None
+    polygon_points = []
+
+    fig, ax = plt.subplots()  # figsize=(3.2, 3.2)) if image size = 320 * 320
+    for point in labels:
+        x, y, label = point
+
+        if label != current_label:
+            if polygon_points:
+                polygon = Polygon(polygon_points, closed=True, alpha=0.5, facecolor=color)
+                ax.add_patch(polygon)
+            polygon_points = []
+            current_label = label
+            color = 'green' if label == 0 else 'yellow'
+
+        polygon_points.append([x, y])
+
+    if polygon_points:
+        polygon = Polygon(polygon_points, closed=True, alpha=0.5, facecolor=color)
+        ax.add_patch(polygon)
+    ax.set_facecolor('black')
+    ax.set_xlim(0, 320)  # Set x-axis limits
+    ax.set_ylim(0, 320)
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+    ax.set_title('Label Mask')
+    # plt.show()        # for show mask
+
+    fig.canvas.draw()
+    image_array = np.frombuffer(fig.canvas.tostring_rgb(), dtype=np.uint8)
+    image_array = image_array.reshape(fig.canvas.get_width_height()[::-1] + (3,))
+    image_array = torch.from_numpy(image_array)
+    plt.close(fig)  # Close the figure to free up resources
+
+    return image_array
 
 
-def func(batch):  # https://blog.csdn.net/weixin_44326452/article/details/123015556
+def func(batch):
     img, label = zip(*batch)
+    img_list = []
+    label_list = []
     for i, l in enumerate(label):
-        l[:, 0] = i
-    return torch.stack(img, 0), torch.cat(label, 0)
+        img_ptr = torch.full((l.size(0), 1), i, dtype=torch.int32)
+        l = torch.cat((img_ptr, l), dim=1)
+        return [torch.stack(img, 0), torch.cat(label, 0)]
 
 
 class Read_voc(Dataset):
@@ -146,7 +213,7 @@ class Read_voc(Dataset):
             self.anno_idx.append(self.anno_path + name + '.xml')
 
     def __getitem__(self, item):
-        print(self.img_idx)
+
         img = Image.open(self.img_idx[item])
         img = transforms.ToTensor()(img)
         normalize(img)
@@ -157,21 +224,25 @@ class Read_voc(Dataset):
         la = []
 
         if os.path.exists(self.root_path):
-            bboxes, labels, width, height = parse_xml(self.anno_idx[item])
+            bboxes, labels, width, height, polygons = parse_xml(self.anno_idx[item])
 
             lbls = convert_label(labels)  # Convert label to number using convert_label() function
-            res.append(bboxes)
-            res.append(lbls)
-            for bbox, label in zip(*res):
-                result.append(bbox + [label])
-            la.append(lbls)
 
+            # res.append(bboxes)
+            res.append(polygons)
+            res.append(lbls)
+
+            for polygons, label in zip(*res):
+                polygons_with_label = [polygons + [label] for polygons in polygons]
+                result.extend(polygons_with_label)
 
         else:
             raise Exception('Path does not Exist!')
 
+        la.append(lbls)
         result = torch.from_numpy(np.array(result))
-        """print(result.size())    #check how many label of one images"""
+
+        # print(result.size())  # check how many label of one images  e.g [239,3]
 
         return img, result
 
@@ -179,27 +250,38 @@ class Read_voc(Dataset):
         return len(self.img_idx)
 
 
+import torch.nn.functional as F
+
+
+# Train Part
 def train_loop(dataloader, model, loss_fn, optimizer, device):
     size = len(dataloader.dataset)
     model.train()
     for batch, (X, y) in enumerate(dataloader):
-        # Move data to the same device as the model
+        # Move data using memory from GPU for the model
         X = X.to(device)
+        y = polygon_to_mask(y)
+        y = np.transpose(y, (2, 0, 1))
+        y = normalize(y)
+        y = y[None, :]
         y = y.to(device)
+        # print(X)
+        # print(y.size())
 
         # Compute prediction and loss
-        pred = model(X)
-
-        loss = loss_fn(pred, y)
+        predict = model(X)
+        # show_Unet_result(predict)  #for showing after Unet image """
+        # print(predict.size())
+        loss = loss_fn(predict, y)
 
         # Backpropagation
         loss.backward()
         optimizer.step()
         optimizer.zero_grad()
 
-        if batch % 100 == 0:
-            loss, current = loss.item(), (batch + 1) * len(X)
-            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+
+        loss, current = loss.item(), (batch + 1) * len(X)
+        print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
 
 
 def test_loop(dataloader, model, loss_fn, device):
@@ -212,101 +294,69 @@ def test_loop(dataloader, model, loss_fn, device):
         for X, y in dataloader:
             # Move data to the same device as the model
             X = X.to(device)
+            y = polygon_to_mask(y)
+            y = np.transpose(y, (2, 0, 1))
+            y = normalize(y)
+            y = y[None, :]
             y = y.to(device)
 
-            pred = model(X)
-            test_loss += loss_fn(pred, y).item()
-            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+            predict = model(X)
+            test_loss += loss_fn(predict, y).item()
+            correct += (predict.argmax(1) == y).type(torch.float).sum().item()
 
     test_loss /= num_batches
     correct /= size
     print(f"Test Error: \n Accuracy: {(100 * correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
 
 
-"""def train(net, train_iter, test_iter, loss, trainer, num_epochs, scheduler, device):
-    num_epochs = 1
-    while True:
-        for inputs, labels in train_iter:
-            inputs, labels = inputs.to(device), labels.to(device)
-            out = net(inputs)
-            loss = loss(out, labels)
-            # 后向
-            print(loss)
-            trainer.zero_grad()
-            loss.backward()
-            trainer.step()
-
-            # 输入的图像，取第一张
-            x = inputs[0]
-            # 生成的图像，取第一张
-            x_ = out[0]
-            # 标签的图像，取第一张
-            y = labels[0]
-            # 三张图，从第0轴拼接起来，再保存
-            img = torch.stack([x, x_, y], 0)
-            save_image(img.cpu(),
-                       os.path.join("C:/Users/willi/PycharmProjects/modelling/Dataset/", f"{num_epochs}.png"))
-            print("image save successfully !")
-
-        print(f"\nEpoch: {num_epochs}/{stop_value}, Loss: {loss}")"""
-
-import segmentation_models_pytorch as smp
-
-
 def main():
-    root_dir = "C:/Users/willi/PycharmProjects/modelling/Dataset/test/"
+    root_dir = "C:/Users/willi/PycharmProjects/modelling/Dataset/train/"
     train_data = Read_voc(root_path=root_dir)  # DataSet Preprocessing
     device = check_cpu()  # checking the training module is using cpu/gpu?
-    """print(type(res))
+    # display_image_with_polygon(train_data)                                        # Display image and label.
+    """img, label = train_data[0]
     print(img.size())
-    print(train_data.__len__())"""
-    train_dataset, test_dataset = random_split(
+    print(label.size())
+    print(train_data.__len__())
+    display_image_with_boxes(img, res, label)"""
+    train_dataset, test_dataset = random_split(     # 70% for train set , 30% for test set
         dataset=train_data,
-        lengths=[1, 1],
+        lengths=[90, 60],
         generator=torch.Generator().manual_seed(0)
     )
-    # display_image_with_boxes(img, res, lbls)
-    # Display image and label.
-    size = 5
-    train_dataloader = DataLoader(train_dataset, batch_size=size, shuffle=True, collate_fn=func)
-    test_dataloader = DataLoader(test_dataset, batch_size=size, shuffle=True, collate_fn=func)
 
-    count = 0
-    """for index, data in enumerate(train_dataloader):
-        feature, labels = data
-        print(feature.size())    # 5,3,480,640
+    # Pytorch load Data
+    b_size = 1
+    train_dataloader = DataLoader(train_dataset, batch_size=b_size, shuffle=True, collate_fn=func)
+    test_dataloader = DataLoader(test_dataset, batch_size=b_size, shuffle=True, collate_fn=func)
+
+    """ count = 0
+    # Display image and label.
+    for index, data in enumerate(train_dataloader):
+        features, labels = data
+        print(f"Feature batch shape: {features.size()}")
+        print(f"Labels batch shape: {labels.size()}")
         count = count + 1
     print(count) """
     # print(len(train_dataloader))         #check the integrity of dataset loader
     # print(len(test_dataloader))
 
     # Model Configuration
-    """model = unet.UNet(n_channels=3, n_classes=1 + 3, bilinear=True).to(device)
-    optimizer = optim.RMSprop(model.parameters(),
-                              lr=1e-5, weight_decay=1e-8, momentum=0.999)
+    CLASSES = ['stem', 'leaf', 'soil']
 
-    scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'max', patience=5)  # goal: maximize Dice score
-    grad_scaler = torch.cuda.amp.GradScaler(enabled=False)
-    loss_fn = nn.CrossEntropyLoss() if model.n_classes > 1 else nn.BCEWithLogitsLoss()
-    epochs_num = 5
-    # Train start
-    train_loop(train_dataloader, model, loss_fn, optimizer, device)
-    # train(model, train_dataloader, test_dataloader, criterion, optimizer, epochs_num, scheduler, device) """
-
-    CLASSES = ['stem', 'soil', 'leaf']
-
-    model = unet.UNet(n_channels=3, n_classes=3)
+    model = unet.UNet(n_channels=3, n_classes=len(CLASSES))
     model = model.cuda()
-
-    loss_fn = torch.nn.BCEWithLogitsLoss()
+    loss_fn = nn.CrossEntropyLoss() if model.n_classes > 1 else nn.BCEWithLogitsLoss()
     optimizer = torch.optim.SGD(model.parameters(), lr=1e-5)
 
-    epochs = 5
+    epochs = 1
     for t in range(epochs):
+        start_time = time.time()
         print(f"Epoch {t + 1}\n-------------------------------")
         train_loop(train_dataloader, model, loss_fn, optimizer, device)
         test_loop(test_dataloader, model, loss_fn, device)
-    print("Done!")
+        end_time = time.time()
+        print('————Train time in this epoch: ————{:.2f}'.format((end_time - start_time)), 'minutes')
 
 
 main()
